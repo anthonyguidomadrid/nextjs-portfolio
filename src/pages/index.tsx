a70da57608ab25a4670f4bef31aff6c9a6f0e2c8
Anthony Guido <anthony.guido@mifarma.es>
2024-06-03 18:19:37 +0200

    chore: removes i18n

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { useTranslation } from "react-i18next";
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Home;
