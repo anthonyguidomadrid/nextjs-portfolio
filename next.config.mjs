c979a90d5ad00a199167ea2fa9253ee030cacdc0
Anthony Guido <anthony.guido@mifarma.es>
2024-06-04 10:14:51 +0200

    fix: hydration issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 import i18nConfig from './next-i18next.config.js'
 
-
 const nextConfig = {
-    reactStrictMode: true,
-    i18n: i18nConfig.i18n
+    i18n: i18nConfig.i18n,
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,36 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useRouter } from "next/router";
+import { ChangeEvent, ChangeEventHandler } from "react";
 
 const Home = () => {
   const { t } = useTranslation("common");
+  const router = useRouter();
+  const { locale } = router;
+
+  const changeLanguage = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newLocale = e.target.value;
+    router.push(router.pathname, router.asPath, { locale: newLocale });
+  };
 
   return (
     <div>
       <h1>{t("home")}</h1>
       <p>Hello</p>
+      <select value={locale} onChange={changeLanguage}>
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
     </div>
   );
 };
 
-export default Home;
-
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
     },
   };
 }
+
+export default Home;
