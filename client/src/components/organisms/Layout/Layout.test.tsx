56e2e96127e6bcb48e1ad9ff125b8dacdbd302b9
Anthony Guido <guido.anthony@orange.fr>
2024-11-12 15:55:34 +0100

    feat: add tests Layout

---
+++ client/src/components/organisms/Layout/Layout.test.tsx
@@ -0,0 +1,26 @@
+import { render } from '~/utils/test-utils';
+import { Layout } from './Layout';
+import { MENU_ITEMS_MOCK } from '~/components/molecules/Header/Header.mocks';
+import { screen } from '@testing-library/react';
+
+describe('Layout', () => {
+  beforeEach(() => {
+    render(
+      <Layout menuItems={MENU_ITEMS_MOCK}>
+        <p>This is my content</p>
+      </Layout>
+    );
+  });
+
+  it('render the header', () => {
+    expect(screen.getByRole('navigation')).toBeVisible();
+  });
+
+  it('renders the content', () => {
+    expect(screen.getByText('This is my content')).toBeVisible();
+  });
+
+  it('renders the footer', () => {
+    expect(screen.getByText('footer')).toBeVisible();
+  });
+});
