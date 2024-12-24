f7a44274142e7ef416d91b9120a886576df1922e
Anthony Guido <guido.anthony@orange.fr>
2024-12-24 12:58:35 +0100

    chore: refactor styling

---
+++ client/src/components/molecules/PortfolioCard/PortfolioCard.styles.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardMedia, styled } from '@mui/material';
+
+export const StyledPortfolioCard = styled(Card)(() => ({
+  position: 'relative',
+  overflow: 'hidden',
+  borderRadius: 0,
+  '&:hover .hover-content': {
+    height: '100%',
+    opacity: 1,
+  },
+}));
+
+export const PortfolioCardContent = styled(CardContent)(({ theme }) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  height: '30%',
+  backgroundColor: theme.palette.background.default,
+  opacity: 0.8,
+  transition: 'all 0.4s ease-in-out',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'start',
+  gap: theme.spacing(0.5),
+}));
--- client/src/components/molecules/PortfolioCard/PortfolioCard.tsx
+++ client/src/components/molecules/PortfolioCard/PortfolioCard.tsx
@@ -1,15 +1,13 @@
-import {
-  Card,
-  Typography,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-} from '@mui/material';
+import { Typography, CardActionArea, CardMedia } from '@mui/material';
 import { PorfolioCardProps } from './PortfolioCard.types';
 import { getMediaUrl } from '~/utils/getMediaUrl';
 import { formatDate } from '~/utils/formatDates';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import {
+  PortfolioCardContent,
+  StyledPortfolioCard,
+} from './PortfolioCard.styles';
 
 export const PortfolioCard: React.FC<PorfolioCardProps> = ({
   title,
@@ -22,69 +20,24 @@ export const PortfolioCard: React.FC<PorfolioCardProps> = ({
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Card
-      sx={{
-        position: 'relative',
-        overflow: 'hidden',
-        borderRadius: 0,
-        '&:hover .hover-content': {
-          height: '100%', // Expand to cover full card
-          opacity: 1, // Ensure content is visible
-        },
-      }}
+    <StyledPortfolioCard
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <CardActionArea>
-        {/* CardMedia */}
         <CardMedia
           component='img'
-          height='300'
-          image={getMediaUrl(thumbnailImg?.url)}
+          src={getMediaUrl(thumbnailImg?.url)}
           alt={thumbnailImg?.alternativeText ?? title}
-          sx={{
-            position: 'relative',
-            objectFit: 'cover',
-            width: '100%',
-            height: '100%',
-          }}
         />
-
-        <CardContent
-          className='hover-content'
-          sx={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            height: '30%',
-            backgroundColor: 'rgba(0, 0, 0, 0.8)',
-            color: '#fff',
-            opacity: 0.8,
-            transition: 'all 0.4s ease-in-out', // Smooth transition
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'start',
-            gap: '0.2rem',
-          }}
-        >
+        <PortfolioCardContent className='hover-content'>
           <Typography variant='caption'>{formatDate(date, locale)}</Typography>
           <Typography gutterBottom variant='h4'>
             {title}
           </Typography>
-          {isHovered && (
-            <Typography
-              variant='body2'
-              sx={{
-                transition: 'opacity 0.4s ease-in-out', // Smooth transition
-              }}
-            >
-              {subTitle}
-            </Typography>
-          )}
-        </CardContent>
+          {isHovered && <Typography variant='body2'>{subTitle}</Typography>}
+        </PortfolioCardContent>
       </CardActionArea>
-    </Card>
+    </StyledPortfolioCard>
   );
 };
