e8980247caed163ea41c7856de852874b8b42776
Anthony Guido <guido.anthony@orange.fr>
2025-01-27 21:16:09 +0100

    fix: build

--- client/src/components/molecules/Timeline/Timeline.styles.tsx
+++ client/src/components/molecules/Timeline/Timeline.styles.tsx
@@ -1,15 +1,13 @@
-import { timelineContentClasses } from '@mui/lab/TimelineContent';
 import Timeline from '@mui/lab/Timeline';
-import { timelineItemClasses } from '@mui/lab/TimeLineItem';
 import { styled } from '@mui/material';
 
 export const StyledTimeLine = styled(Timeline)(({ theme }) => ({
   padding: 0,
-  [`& .${timelineItemClasses.root}:before`]: {
+  [`& .MuiTimelineItem-root:before`]: {
     flex: 0,
     padding: 0,
   },
-  [`& .${timelineContentClasses.root}`]: {
+  [`& .MuiTimelineContent-root`]: {
     padding: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
       padding: theme.spacing(3),
--- client/src/components/molecules/Timeline/Timeline.tsx
+++ client/src/components/molecules/Timeline/Timeline.tsx
@@ -1,12 +1,12 @@
-import MuiTimeline from '@mui/lab/Timeline';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimeLineItem, { timelineItemClasses } from '@mui/lab/TimeLineItem';
+import TimeLineItem from '@mui/lab/TimeLineItem';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import { ResumeItem } from '~/components/atoms';
 import { TimelineProps } from './Timeline.types';
 import { StyledTimeLine } from './Timeline.styles';
+import { ComponentMainResumeItem } from '~/generated/graphql';
 
 export const Timeline: React.FC<TimelineProps> = ({ resumeItems }) => {
   return (
@@ -18,7 +18,7 @@ export const Timeline: React.FC<TimelineProps> = ({ resumeItems }) => {
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            {resumeItem && <ResumeItem {...resumeItem} />}
+            {resumeItem && <ResumeItem {...(resumeItem as ComponentMainResumeItem)} />}
           </TimelineContent>
         </TimeLineItem>
       ))}
