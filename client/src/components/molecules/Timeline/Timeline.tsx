ad761fd75d721837c4081dfa5d107e4c31d17c41
Anthony Guido <guido.anthony@orange.fr>
2025-01-27 21:41:56 +0100

    chore: update Timeline

--- client/src/components/molecules/Timeline/Timeline.tsx
+++ client/src/components/molecules/Timeline/Timeline.tsx
@@ -1,26 +1,25 @@
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimeLineItem from '@mui/lab/TimeLineItem';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import { ResumeItem } from '~/components/atoms';
 import { TimelineProps } from './Timeline.types';
 import { StyledTimeLine } from './Timeline.styles';
-import { ComponentMainResumeItem } from '~/generated/graphql';
+import { TimelineItem } from '@mui/lab';
 
 export const Timeline: React.FC<TimelineProps> = ({ resumeItems }) => {
   return (
     <StyledTimeLine>
       {resumeItems?.map((resumeItem) => (
-        <TimeLineItem key={resumeItem?.title}>
+        <TimelineItem key={resumeItem?.title}>
           <TimelineSeparator>
             <TimelineDot />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            {resumeItem && <ResumeItem {...(resumeItem as ComponentMainResumeItem)} />}
+            {resumeItem && <ResumeItem {...resumeItem } />}
           </TimelineContent>
-        </TimeLineItem>
+        </TimelineItem>
       ))}
     </StyledTimeLine>
   );
--- client/src/components/molecules/Timeline/Timeline.types.ts
+++ client/src/components/molecules/Timeline/Timeline.types.ts
@@ -1,5 +1,5 @@
 import { ComponentMainResumeItem } from '~/generated/graphql';
 
 export type TimelineProps = {
-  resumeItems?: (Omit<ComponentMainResumeItem, 'id'> | null)[];
+  resumeItems?: (ComponentMainResumeItem | null)[];
 };
