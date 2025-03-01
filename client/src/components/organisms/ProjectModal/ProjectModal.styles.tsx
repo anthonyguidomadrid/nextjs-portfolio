5ef679d6bfb90a3fc51469bb43f1fd9a2a729a94
Anthony Guido <guido.anthony@orange.fr>
2025-03-01 09:49:30 +0100

    chore: refactor project modal

--- client/src/components/organisms/ProjectModal/ProjectModal.styles.tsx
+++ client/src/components/organisms/ProjectModal/ProjectModal.styles.tsx
@@ -1,28 +1,5 @@
-import { Box, Dialog, DialogContent, Grid, styled } from '@mui/material';
+import { Grid, styled } from '@mui/material';
 
-export const StyledDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiPaper-root': {
-    backgroundColor: theme.palette.common.black,
-    backgroundImage: 'none',
-    minHeight: '80vh',
-  },
-}));
-
-export const MobileModalNavWrapper = styled(Grid)(({ theme }) => ({
+export const ModalNavWrapper = styled(Grid)(({ theme }) => ({
   padding: theme.spacing(2),
-}));
-
-export const StyledDialogContent = styled(DialogContent)(({ theme }) => ({
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  gap: theme.spacing(2),
-  marginBottom: theme.spacing(2),
-}));
-
-export const StyledDialogContentText = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: theme.spacing(2),
 }));
--- client/src/components/organisms/ProjectModal/ProjectModal.tsx
+++ client/src/components/organisms/ProjectModal/ProjectModal.tsx
@@ -1,25 +1,24 @@
 import {
   Button,
+  Dialog,
   DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Fade,
   Grid,
   Typography,
   useMediaQuery,
 } from '@mui/material';
 import { ProjectModalProps } from './ProjectModal.types';
-import { formatDate, getAllCategoryName } from '~/utils';
+import { getAllCategoryName } from '~/utils';
 import { ModalNavigation, RichText } from '~/components/atoms';
 import Link from 'next/link';
 import { ImageCarousel } from '~/components/molecules';
 import { useTranslation } from 'next-i18next';
 import theme from '~/utils/theme';
 import CloseIcon from '@mui/icons-material/Close';
-import {
-  MobileModalNavWrapper,
-  StyledDialog,
-  StyledDialogContent,
-  StyledDialogContentText,
-} from './ProjectModal.styles';
+import { ModalNavWrapper } from './ProjectModal.styles';
 import { useRouter } from 'next/router';
 
 export const ProjectModal: React.FC<ProjectModalProps> = ({
@@ -45,74 +44,54 @@ export const ProjectModal: React.FC<ProjectModalProps> = ({
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
-    <StyledDialog
+    <Dialog
+      fullScreen={isMobile}
       open={open}
       onClose={handleClose}
-      maxWidth='lg'
-      fullScreen={isMobile}
       TransitionComponent={Fade}
       keepMounted
     >
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          {isMobile && (
-            <MobileModalNavWrapper container justifyContent='space-between'>
-              <Grid item>
-                <ModalNavigation
-                  isFirst={isFirst}
-                  isLast={isLast}
-                  onPrev={onPrev}
-                  onNext={onNext}
-                />
-              </Grid>
-              <Grid item>
-                <Button onClick={handleClose}>
-                  <CloseIcon />
-                </Button>
-              </Grid>
-            </MobileModalNavWrapper>
-          )}
-          <ImageCarousel images={pictures.data} />
+      <ModalNavWrapper container justifyContent='space-between'>
+        <Grid item>
+          <ModalNavigation
+            isFirst={isFirst}
+            isLast={isLast}
+            onPrev={onPrev}
+            onNext={onNext}
+          />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <StyledDialogContent>
-            {!isMobile && (
-              <ModalNavigation
-                isFirst={isFirst}
-                isLast={isLast}
-                onPrev={onPrev}
-                onNext={onNext}
-              />
-            )}
-            <Typography>{formatDate(date, locale)}</Typography>
-            <Typography variant='h2'>{title}</Typography>
-            <Typography variant='h3'>{subTitle}</Typography>
-            <StyledDialogContentText>
-              <RichText content={description} />
-              <Typography variant='caption'>
-                {t('project.modal.categories', {
-                  categories: categories?.data
-                    .map((category) => category.attributes?.Tag)
-                    .filter(
-                      (category) => category !== getAllCategoryName(locale)
-                    )
-                    .join(', '),
-                })}
-              </Typography>
-            </StyledDialogContentText>
-          </StyledDialogContent>
-          <DialogActions>
-            <Link href={codeUrl} target='_blank'>
-              <Button>{t('project.modal.button.code-url')}</Button>
-            </Link>
-            {projectUrl && (
-              <Link href={projectUrl} target='_blank'>
-                <Button autoFocus>{t('project.modal.button.demo-url')}</Button>
-              </Link>
-            )}
-          </DialogActions>
+        <Grid item>
+          <Button onClick={handleClose}>
+            <CloseIcon />
+          </Button>
         </Grid>
-      </Grid>
-    </StyledDialog>
+      </ModalNavWrapper>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <ImageCarousel images={pictures.data} />
+        <Typography variant='h3'>{subTitle}</Typography>
+        <DialogContentText>
+          <RichText content={description} />
+          <Typography variant='caption'>
+            {t('project.modal.categories', {
+              categories: categories?.data
+                .map((category) => category.attributes?.Tag)
+                .filter((category) => category !== getAllCategoryName(locale))
+                .join(', '),
+            })}
+          </Typography>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Link href={codeUrl} target='_blank'>
+          <Button>{t('project.modal.button.code-url')}</Button>
+        </Link>
+        {projectUrl && (
+          <Link href={projectUrl} target='_blank'>
+            <Button autoFocus>{t('project.modal.button.demo-url')}</Button>
+          </Link>
+        )}
+      </DialogActions>
+    </Dialog>
   );
 };
