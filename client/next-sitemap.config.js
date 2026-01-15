cd9b890074bd2eba63df6ac30aa45d27631b7fb9
Anthony Guido <guido.anthony@orange.fr>
2026-01-15 11:15:02 +0100

    chore: update sitemap

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL,
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-  };
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  exclude: ['/404', '/404.html'],
+  additionalPaths: async () => {
+    const cvLocales = ['en', 'es', 'fr'];
+    return cvLocales.map((locale) => ({
+      loc: `/cv/${locale}/AG_CV_2025.pdf`,
+    }));
+  },
+};
