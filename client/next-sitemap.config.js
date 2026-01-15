bbc8aa4955f159dcd6727300ae3cd66eb5f98624
Anthony Guido <guido.anthony@orange.fr>
2026-01-15 11:35:31 +0100

    chore: update sitemap

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
+const fs = require('fs');
+const path = require('path');
 const i18nConfig = require('./next-i18next.config.js');
 
 const locales = Array.isArray(i18nConfig?.i18n?.locales)
@@ -10,10 +12,31 @@ module.exports = {
   generateRobotsTxt: false,
   outDir: 'public/sitemaps',
   sitemapSize: 5000,
-  exclude: ['/*404*','/**/404*'],
+  exclude: ['/*404*', '/**/404*'],
   additionalPaths: async () => {
-    return locales.map((locale) => ({
-      loc: `/cv/${locale}/AG_CV_2025.pdf`,
-    }));
+    return locales.map((locale) => {
+      const loc = `/cv/${locale}/AG_CV_2025.pdf`;
+      const pdfPath = path.join(
+        process.cwd(),
+        'public',
+        'cv',
+        locale,
+        'AG_CV_2025.pdf'
+      );
+
+      let lastmod;
+      try {
+        lastmod = fs.statSync(pdfPath).mtime.toISOString();
+      } catch {
+        // If the file doesn't exist at build time, omit lastmod.
+      }
+
+      return {
+        loc,
+        lastmod,
+        changefreq: 'yearly',
+        priority: 0.3,
+      };
+    });
   },
 };
