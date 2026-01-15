55d5d35767e0034056a97a484dba51c3e44894a2
Anthony Guido <guido.anthony@orange.fr>
2026-01-15 11:27:44 +0100

    fix: sitemap generation

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
+const i18nConfig = require('./next-i18next.config.js');
+
+const locales = Array.isArray(i18nConfig?.i18n?.locales)
+  ? i18nConfig.i18n.locales
+  : [];
+
 module.exports = {
   siteUrl: process.env.SITE_URL,
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
+  outDir: 'public/sitemaps',
   sitemapSize: 5000,
-  exclude: ['/404', '/404.html'],
+  exclude: ['/*404*','/**/404*'],
   additionalPaths: async () => {
-    const cvLocales = ['en', 'es', 'fr'];
-    return cvLocales.map((locale) => ({
+    return locales.map((locale) => ({
       loc: `/cv/${locale}/AG_CV_2025.pdf`,
     }));
   },
